f7ee5a8e731bebb83999920dc78f7c203a475368
manojchikkumbi <chikkumbir@gmail.com>
2025-11-21 11:44:08 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,8 +1,5 @@
 import sys
 
-# Default values
-num1 = 15
-num2 = 20
 
 # If user provides two arguments, use them instead of defaults
 if len(sys.argv) == 3:
@@ -10,13 +7,15 @@ if len(sys.argv) == 3:
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
 else:
+    
     print("No arguments provided — using default values (15 and 20).")
+# Default values
+    num1 = 15
+    num2 = 20
+    
 
 # Find sum
 total = num1 + num2
-
-# Print script name
-script_name = sys.argv[0]
 
 # Check even/odd
 if total % 2 == 0:
